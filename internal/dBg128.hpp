6ecb1461ed09af0bc333f6a97e5786c9e39ef8e7
nicolaprezza <nicolapr@gmail.com>
2021-08-23 15:29:15 +0200

    switching to 64 bits

--- internal/dBg128.hpp
+++ internal/dBg128.hpp
@@ -25,14 +25,12 @@
 
 using namespace std;
 
-using kmer_t = uint64_t;
+using kmer_t = __uint128_t;
 
 namespace dbg{
 
 enum format_t {fasta, fastq};
 typedef pair<uint64_t,uint8_t> edge_t;
-
-const int sigma = 8;//alphabet size (see toINT; we don't actually use all characters)
 
 /*
  * convert DNA alphabet {$,A,C,G,T} to integers in [0,4] (3 bits per int)
@@ -40,29 +38,16 @@ const int sigma = 8;//alphabet size (see toINT; we don't actually use all charac
 uint8_t toINT(char c){
 
 	switch(c){
-		case '$': return 0; break;				// 000
-		case 'A': case 'a': return 4; break;	// 100
-		case 'C': case 'c': return 5; break;	// 101
-		case 'G': case 'g': return 6; break;	// 110
-		case 'T': case 't': return 7; break;	// 111
+		case '$': return 0; break;
+		case 'A': case 'a': return 1; break;
+		case 'C': case 'c': return 2; break;
+		case 'G': case 'g': return 3; break;
+		case 'T': case 't': return 4; break;
 		default:break;
 
 	}
 
-	return 4;//this includes 'N' characters.
-}
-
-/*
- * keeps only the last 2 bits of the above encoding:
- * $ = A = 00
- * C = 01
- * G = 10
- * T = 11
- */
-uint8_t from3_to2_bits(uint8_t c){
-
-	return c & uint8_t(3);
-
+	return 1;//this includes 'N' characters.
 }
 
 //inverse of the above
@@ -70,10 +55,10 @@ char toCHAR(uint8_t x){
 
 	switch(x){
 		case 0: return '$'; break;
-		case 4: return 'A'; break;
-		case 5: return 'C'; break;
-		case 6: return 'G'; break;
-		case 7: return 'T'; break;
+		case 1: return 'A'; break;
+		case 2: return 'C'; break;
+		case 3: return 'G'; break;
+		case 4: return 'T'; break;
 		default:break;
 
 	}
@@ -82,143 +67,57 @@ char toCHAR(uint8_t x){
 
 }
 
-//2-bits format to char
-char toCHAR2(uint8_t x){
+//format example: if k=3 and we have a kmer ACG followed by T, then an edge is represented as an integer rev(ACG)T = GCAT
 
-	switch(x){
-		case 0: return 'A'; break;
-		case 1: return 'C'; break;
-		case 2: return 'G'; break;
-		case 3: return 'T'; break;
-		default:break;
+/*
+ * input: edge (XYZ,W) stored as integer of 128 bits (see "format example" above), character c stored in 3 bits (see function toINT), and order k
+ * output: edge (YZW,c)
+ */
+kmer_t edge(kmer_t kmer, uint8_t c, uint8_t k){
 
-	}
-
-	return 'A';
+	return (((kmer >> 3) | ((kmer & kmer_t(7))<<(3*k))) & (~kmer_t(7))) | c;
 
 }
 
 /*
- * Format:
- *
- * A kmer_t (64 bits, 8 bytes) encodes a k-mer (k max = 28), plus one character following the kmer. E.g. (ACGG,T)
- *
- * - The kmer can be padded on the left by $ signs. E.g. with k = 5:  $$$AG
- * - apart from the padding, all characters must be A,C,G,T
- * - finally, the extra character can be A,C,G,T, or $.
- *
- * The first 7 bytes encode the at most 28 bases of the kmer. This is the format:
- * - $ = A = 00, C = 01, G = 10, T = 11
- * - the kmer is stored in reversed order, in the least significant bits
- *
- * Example: if k = 5 and the kmer is $$AGT, we will store the number code(TGA$$) = 11 10 00 00 00
- *
- * The last byte is composed of:
- *
- * - 5 bits encoding the number of characters different than $ in the kmer (from 0 to k)
- * - 3 bits encoding the extra character (A,C,G,T, or $), encoded with function toINT
- *
- * This encoding has the property that, by sorting objects kmer_t, we sort the kmers co-lexicographically in the order given by toINT()
- * This happens because, even if $ and A are both encoded as 00, we also store how many characters different than $ precede the kmer.
- * Thus, in kmers with a longer padding this number will be smaller.
- *
+ * input: edge (XYZ,W) stored as integer of 128 bits (see "format example" above)
+ * output: just the kmer XYZ (stored in reverse order as ZYX as seen above)
  */
+kmer_t get_kmer(kmer_t kmer){
 
+	return kmer >> 3;
 
+}
 
 /*
- * input: kmer_t (XYZ,W),
- * output: char W
+ * input: char c
+ * output: the kmker ($$$,c)
+ */
+kmer_t init_kmer(__uint8_t c){
+
+	return c;
+
+}
+
+/*
+ * input: edge (XYZ,W) stored as integer of 128 bits (see "format example" above)
+ * output: character Z
+ */
+uint8_t last_char(kmer_t kmer, uint8_t k){
+
+	return (kmer >> (3*k)) & kmer_t(7);
+
+}
+
+/*
+ * input: edge (XYZ,W) stored as integer of 128 bits (see "format example" above)
+ * output: character W on the edge
  */
 char get_edge(kmer_t kmer){
 
 	return toCHAR(kmer & kmer_t(7));
 
 }
-
-uint8_t get_counter(kmer_t kmer){
-
-	return (kmer >> 3) & uint8_t(31);
-
-}
-
-/*
- * input: kmer_t (XYZ,W),
- * output: just the kmer XYZ, without counter and without extra char
- */
-kmer_t get_kmer(kmer_t kmer){
-
-	return kmer >> 8;
-
-}
-
-//check if the two kmers are equal (excludes extra character)
-bool equal_kmers(kmer_t u, kmer_t v){
-
-	return (u>>3) == (v>>3);
-
-}
-
-/*
- * input: kmer_t (XYZ,W), character c stored in 3 bits (see function toINT), and order k
- * assumption: W is not $
- * output: kmer_t (YZW,c)
- */
-kmer_t edge(kmer_t kmer, char ch, uint8_t k){
-
-	uint8_t c = toINT(ch);
-
-	assert(get_edge(kmer) != '$');
-
-	uint64_t N = get_counter(kmer);
-	N = N==k ? k : N+1;
-
-	uint64_t km = get_kmer(kmer);
-
-	uint64_t W = kmer & uint64_t(3); //2-bits per base format
-
-	km = (km >> 2) | (W << (2*(k-1)));
-
-	return (km<<8) | (N << 3) | uint64_t(c);
-
-}
-
-
-
-/*
- * input: char c (toINT)
- * output: the kmer ($$$,c)
- */
-kmer_t init_kmer(char c){
-
-	return toINT(c);
-
-}
-
-/*
- * input: kmer_t (XYZ,W)
- * output: character Z
- */
-char last_char(kmer_t kmer, uint8_t k){
-
-	kmer = kmer >> 8;
-
-	return toCHAR(((kmer >> (2*(k-1))) & uint64_t(3)) | uint64_t(4) );
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 
 string kmer_to_str_(kmer_t kmer, int k){
 
@@ -235,37 +134,32 @@ string kmer_to_str_(kmer_t kmer, int k){
 
 }
 
-*/
-
 string kmer_to_str(kmer_t kmer, int k){
 
-	uint64_t km = get_kmer(kmer);
-	uint64_t N = get_counter(kmer);
-	char c = get_edge(kmer);
+	char edge = toCHAR(kmer & kmer_t(7));
 
-	string out;
+	string km;
 
-	int padding = k-N;//number of trailing $
+	kmer = kmer >> 3;
 
-	for(int i=0;i<padding;++i) out += '$';
+	for(int i=0;i<k;++i){
 
-	km = km >> (padding*2);
-
-	for(int i=0;i<N;++i){
-
-		out += toCHAR2(km & uint64_t(3));
-		km = km>>2;
+		km += toCHAR(kmer & kmer_t(7));
+		kmer = kmer >> 3;
 
 	}
 
-	return "(" + out + "," + c + ")";
+	km += "|";
+	km += edge;
+
+	return km;
 
 }
 
 //has $ symbols in the kmer part?
-bool has_dollars(kmer_t kmer, uint8_t k){
+bool has_dollars(kmer_t kmer){
 
-	return get_counter(kmer)<k;
+	return ((kmer >> 3) &  kmer_t(7)) == 0;
 
 }
 
@@ -283,7 +177,7 @@ public:
 			uint8_t k = 28,
 			bool verbose = true) : k(k){
 
-		assert(k>0 and k<=28);
+		assert(k>0 and k<=41);
 
 		if(verbose)
 			cout << "Computing how much memory I need to allocate ..." << endl;
@@ -347,20 +241,25 @@ public:
 			// start processing DNA fragment
 
 			//first kmer: ($$$,C), where C is the first letter of str
-			kmer_t kmer = init_kmer(str[0]);
+
+			uint8_t first_char = toINT(str[0]);
+
+			//if(first_char==0) cout << str<<endl;
+			assert(first_char!=0);
+			kmer_t kmer = init_kmer(first_char);
 
 			kmers.push_back(kmer);
 
 			//push the other kmers
 			for(int i=1;i<str.length();++i){
 
-				kmer = edge(kmer, str[i],k);
+				kmer = edge(kmer, toINT(str[i]),k);
 				kmers.push_back(kmer);
 
 			}
 
 			//last kmer: (ACG,$), where ACG was the last kmer in the DNA fragment
-			kmer = edge(kmer, '$',k);
+			kmer = edge(kmer, toINT('$'),k);
 			kmers.push_back(kmer);
 
 			if(format == fastq){
@@ -399,7 +298,7 @@ public:
 			//cout << "out: " << out << endl;
 
 			//if kmer changes or i goes beyond the end of kmers
-			if(i == kmers.size() || not equal_kmers(kmers[i],prev_kmer)){
+			if(i == kmers.size() || get_kmer(kmers[i]) != get_kmer(prev_kmer)){
 
 				assert(i == kmers.size() or out.length() > 0);
 
@@ -435,7 +334,7 @@ public:
 				out = "";
 				if(i < kmers.size()) out += get_edge(kmers[i]);
 
-				if(has_dollars(prev_kmer,k)){
+				if(has_dollars(prev_kmer)){
 
 					padded_node.push_back(true);
 					padded_kmers++;
@@ -468,7 +367,7 @@ public:
 
 			if(get_edge(kmers[i]) != '$'){
 
-				kmers[new_size++] = edge(kmers[i],'$',k);
+				kmers[new_size++] = edge(kmers[i],0,k);
 
 			}
 
@@ -478,18 +377,23 @@ public:
 		kmers.resize(new_size);
 		kmers.shrink_to_fit();
 
-		for(int i=0;i<sigma;++i){
-			C[i] = 0;
-			C_node[i] = 0;
-		}
-
 		//for each letter, count how many incoming edges with that letter we have
 		C[toINT('$')] = 1; // kmer $$$ (with incoming dummy label $) is not in kmers but we must take it into account
+		C[toINT('A')] = 0;
+		C[toINT('C')] = 0;
+		C[toINT('G')] = 0;
+		C[toINT('T')] = 0;
 
 		//for each letter, count how many nodes with that incoming letter we have
 		C_node[toINT('$')] = 1;
+		C_node[toINT('A')] = 0;
+		C_node[toINT('C')] = 0;
+		C_node[toINT('G')] = 0;
+		C_node[toINT('T')] = 0;
 
-		C[toINT(last_char(kmers[0],k))]++;
+		assert(last_char(kmers[0],k) != toINT('$'));
+
+		C[last_char(kmers[0],k)]++;
 		IN.push_back(true); // kmer $$$ is not in kmers but we must take it into account
 
 		prev_kmer = kmers[0];
@@ -498,16 +402,18 @@ public:
 
 			if(i<kmers.size()){
 
-				C[toINT(last_char(kmers[i],k))]++;
+				assert(last_char(kmers[i],k) != toINT('$'));
+				C[last_char(kmers[i],k)]++;
 
 			}
 
 			//if kmer changes or i goes beyond the end of kmers
-			if(i == kmers.size() || not equal_kmers(kmers[i],prev_kmer)){
+			if(i == kmers.size() || get_kmer(kmers[i]) != get_kmer(prev_kmer)){
 
 				IN.push_back(true);
 
-				C_node[toINT(last_char(prev_kmer,k))]++;
+				assert(last_char(prev_kmer,k) != toINT('$'));
+				C_node[last_char(prev_kmer,k)]++;
 
 			}else{
 
@@ -521,20 +427,20 @@ public:
 
 		//cumulate C
 
-		for(int i=sigma-1;i>0;--i) C[i] = C[i-1];
+		for(int i=4;i>0;--i) C[i] = C[i-1];
 		C[0] = 0;
-		for(int i=1;i<sigma;++i) C[i] += C[i-1];
+		for(int i=1;i<5;++i) C[i] += C[i-1];
 
-		for(int i=sigma-1;i>0;--i) C_node[i] = C_node[i-1];
+		for(int i=4;i>0;--i) C_node[i] = C_node[i-1];
 		C_node[0] = 0;
-		for(int i=1;i<sigma;++i) C_node[i] += C_node[i-1];
+		for(int i=1;i<5;++i) C_node[i] += C_node[i-1];
 
-		uint64_t tot_letters[sigma];
-		for(int i=0;i<sigma;++i) tot_letters[i]=0;
+		uint64_t tot_letters[5];
+		for(int i=0;i<5;++i) tot_letters[i]=0;
 
 		for(auto c:BWT) tot_letters[toINT(c)]++;
 
-		assert(C[sigma-1] + tot_letters[sigma-1] == IN.size());
+		assert(C[4] + tot_letters[4] == IN.size());
 
 		/*
 		cout << "BWT/OUT/IN/padded:\n" << BWT << endl;
@@ -596,10 +502,10 @@ public:
 		auto nodes_start = vector<uint64_t>(nr_of_nodes,0);
 		vector<uint64_t> edges;
 
-		uint64_t F_pos[sigma];
-		uint64_t curr_node_in[sigma];
+		uint64_t F_pos[5];
+		uint64_t curr_node_in[5];
 
-		for(int i=0;i<sigma;++i){
+		for(int i=0;i<5;++i){
 			F_pos[i] = C[i];
 			curr_node_in[i] = C_node[i];
  		}
@@ -636,7 +542,7 @@ public:
 
 		edges = vector<uint64_t>(m);
 
-		for(int i=0;i<sigma;++i){
+		for(int i=0;i<5;++i){
 			F_pos[i] = C[i];
 			curr_node_in[i] = C_node[i];
  		}
@@ -726,10 +632,10 @@ public:
 				new_padded_node.push_back(padded_node[i]);
 
 		vector<bool> new_IN;
-		uint64_t new_C[sigma];
-		uint64_t new_C_node[sigma];
+		uint64_t new_C[5];
+		uint64_t new_C_node[5];
 
-		for(int k = 0;k<sigma;++k){
+		for(int k = 0;k<5;++k){
 			new_C[k]=0;
 			new_C_node[k]=0;
 		}
@@ -740,7 +646,7 @@ public:
 		for(uint64_t i=0;i<nr_of_nodes;++i){
 
 			//find incoming letter of node i
-			while(F_char < sigma-1 && not (i >= C_node[F_char] and i <  C_node[F_char+1])) F_char++;
+			while(F_char < 4 && not (i >= C_node[F_char] and i <  C_node[F_char+1])) F_char++;
 
 			if(necessary[i]){
 
@@ -774,19 +680,19 @@ public:
 
 		}
 
-		for(int i=sigma-1;i>0;--i) new_C[i] = new_C[i-1];
+		for(int i=4;i>0;--i) new_C[i] = new_C[i-1];
 		new_C[0] = 0;
-		for(int i=1;i<sigma;++i) new_C[i] += new_C[i-1];
+		for(int i=1;i<5;++i) new_C[i] += new_C[i-1];
 
-		for(int i=sigma-1;i>0;--i) new_C_node[i] = new_C_node[i-1];
+		for(int i=4;i>0;--i) new_C_node[i] = new_C_node[i-1];
 		new_C_node[0] = 0;
-		for(int i=1;i<sigma;++i) new_C_node[i] += new_C_node[i-1];
+		for(int i=1;i<5;++i) new_C_node[i] += new_C_node[i-1];
 
 
 		string new_BWT;
 		vector<bool> new_OUT;
 
-		for(int i=0;i<sigma;++i){
+		for(int i=0;i<5;++i){
 			F_pos[i] = C[i];
 			curr_node_in[i] = C_node[i];
  		}
@@ -855,18 +761,18 @@ public:
 		OUT = new_OUT;
 		IN = new_IN;
 
-		for(int k=0;k<sigma;++k){
+		for(int k=0;k<5;++k){
 			C[k] = new_C[k];
 			C_node[k] = new_C_node[k];
 		}
 
-		uint64_t tot_letters[sigma];
-		for(int i=0;i<sigma;++i) tot_letters[i]=0;
+		uint64_t tot_letters[5];
+		for(int i=0;i<5;++i) tot_letters[i]=0;
 
 		for(auto c:BWT) tot_letters[toINT(c)]++;
 
 
-		assert(C[sigma-1] + tot_letters[sigma-1] == IN.size());
+		assert(C[4] + tot_letters[4] == IN.size());
 
 		/*
 		cout << "BWT/OUT/IN/padded:\n" << BWT << endl;
@@ -909,10 +815,10 @@ public:
 		auto nodes_start = vector<uint64_t>(nr_of_nodes,0);
 		vector<uint64_t> edges;
 
-		uint64_t F_pos[sigma];
-		uint64_t curr_node_in[sigma];
+		uint64_t F_pos[5];
+		uint64_t curr_node_in[5];
 
-		for(int i=0;i<sigma;++i){
+		for(int i=0;i<5;++i){
 			F_pos[i] = C[i];
 			curr_node_in[i] = C_node[i];
  		}
@@ -922,12 +828,12 @@ public:
 
 		//for each letter, store successor of previous node following that letter.
 		//if successor = nr_of_nodes, there is no edge labeled with that letter.
-		uint64_t prev_succ[sigma];
-		for(int i=0;i<sigma;++i) prev_succ[i] = nr_of_nodes;
+		uint64_t prev_succ[5];
+		for(int i=0;i<5;++i) prev_succ[i] = nr_of_nodes;
 
 		//the same for current node
-		uint64_t curr_succ[sigma];
-		for(int i=0;i<sigma;++i) curr_succ[i] = nr_of_nodes;
+		uint64_t curr_succ[5];
+		for(int i=0;i<5;++i) curr_succ[i] = nr_of_nodes;
 
 		vector<bool> MN(nr_of_nodes,true); //MN equivalence relation
 
@@ -972,7 +878,7 @@ public:
 			if(OUT[bwt_pos]){
 
 				//MN[curr_node_out] is true if and only if u and u-1 have the same outgoing letters
-				for(int i=0;curr_node_out>0 and i<sigma;++i){
+				for(int i=0;curr_node_out>0 and i<5;++i){
 
 					bool prev_has_letter = prev_succ[i]!=nr_of_nodes;
 					bool curr_has_letter = curr_succ[i]!=nr_of_nodes;
@@ -983,7 +889,7 @@ public:
 
 				}
 
-				for(int i=0;i<sigma;++i){
+				for(int i=0;i<5;++i){
 
 					prev_succ[i] = curr_succ[i];
 					curr_succ[i] = nr_of_nodes;
@@ -1009,15 +915,15 @@ public:
 
 		edges = vector<uint64_t>(m);
 
-		for(int i=0;i<sigma;++i){
+		for(int i=0;i<5;++i){
 			F_pos[i] = C[i];
 			curr_node_in[i] = C_node[i];
  		}
 
 		curr_node_out = 0;
 
-		for(int i=0;i<sigma;++i) prev_succ[i] = nr_of_nodes;
-		for(int i=0;i<sigma;++i) curr_succ[i] = nr_of_nodes;
+		for(int i=0;i<5;++i) prev_succ[i] = nr_of_nodes;
+		for(int i=0;i<5;++i) curr_succ[i] = nr_of_nodes;
 
 		for(uint64_t bwt_pos=0;bwt_pos<BWT.length();++bwt_pos){
 
@@ -1059,7 +965,7 @@ public:
 
 				curr_node_out++;
 
-				for(int i=0;i<sigma;++i){
+				for(int i=0;i<5;++i){
 
 					prev_succ[i] = curr_succ[i];
 					curr_succ[i] = nr_of_nodes;
@@ -1152,9 +1058,9 @@ private:
 	vector<bool> OUT; //marks (bit 1) the last outgoing edge of each node in BWT order
 	vector<bool> IN;  //marks (bit 1) the last incoming edge of each node in BWT order
 	vector<bool> padded_node; // marks padded (dummy) kmers, i.e. kmers containing a $
-	uint64_t C[sigma];//starting positions of each letter in column F. Accessed as C[toINT(char)]
+	uint64_t C[5];//starting positions of each letter in column F. Accessed as C[toINT(char)]
 
-	uint64_t C_node[sigma]; //C_node[c] = starting positions of nodes with incoming letter c in the list of all nodes.
+	uint64_t C_node[5]; //C_node[c] = starting positions of nodes with incoming letter c in the list of all nodes.
 
 };
 
